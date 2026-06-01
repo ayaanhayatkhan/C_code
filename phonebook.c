6a46d11586db62ff557a7c07686c12ae08c32270
AyaanHayatKhan <ayaanhayatkhan009@gmail.com>
2026-06-01 23:05:30 +0500

    Program

---
+++ phonebook.c
@@ -0,0 +1,20 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string names[] = {"Ayaan", "Hayat", "Khan"};
+    string numbers[] = {"+1-617-495-1000", "+1-949-468-2750", "+1-617-495-1000"};
+    string name = get_string("Name: ");
+ for (int i = 0; i < 3; i++)
+    {
+        if (strcmp(names[i], name) == 0)
+        {
+        printf("found %s\n", numbers[i]);  
+    return 0;
+        }
+    }
+printf("Not found\n");
+return 1;
+}
