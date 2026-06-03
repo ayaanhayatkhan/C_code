48ae94c2bec72d3a2097b265438ad7408f329004
AyaanHayatKhan <ayaanhayatkhan009@gmail.com>
2026-06-03 20:29:10 +0500

    New program of "*"

---
+++ iteration.c
@@ -0,0 +1,20 @@
+#include<cs50.h>
+#include<stdio.h>
+
+int main(void)
+{
+    int height = get_int("Height:");
+
+    for (int i=0 ; i<=height ; i++)
+    {
+    for (int j = 0; j <=i ; j++)
+    {
+    printf("*");  
+      
+    }
+   
+    printf("\n");
+    printf("\t");
+    }
+
+}
